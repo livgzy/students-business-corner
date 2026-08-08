811e11ac3b3bc9d59f37b02bb1f857301f1f6538
Liv <liv.gzy@gmail.com>
2026-08-09 02:12:57 +0700

    ubah config filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,11 +47,24 @@ return [
             'report' => false,
         ],
         
+        // 'tsbc_disk' => [
+        //     'driver' => 'local',
+        //     'root' => 'D:/laragon/www/tenant-sbc/storage/app/public',
+        //     'url' => rtrim(env('TSBC_URL', 'http://localhost'), '/') . '/storage',
+        //     'visibility' => 'public',
+        //     'throw' => false,
+        //     'report' => false,
+        // ],
+
         'tsbc_disk' => [
-            'driver' => 'local',
-            'root' => 'D:/laragon/www/tenant-sbc/storage/app/public',
-            'url' => rtrim(env('TSBC_URL', 'http://localhost'), '/') . '/storage',
-            'visibility' => 'public',
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
         ],
