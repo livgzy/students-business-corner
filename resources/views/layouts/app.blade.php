f6057a3dd3bc801ef2b036a9c6ef2ca5dc36cc25
Liv <liv.gzy@gmail.com>
2026-08-09 02:23:37 +0700

    fixit4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,6 @@
                                         alt="Logo UCIC"
                                         class="h-9 sm:h-18 w-auto object-contain drop-shadow-sm transition-transform duration-200 group-hover:scale-105">
                                 </a>
-                            @endif
                                 <a href="/" class="text-lg sm:text-xl font-bold text-orange-600 tracking-tight">
                                     Student Business Corner
                                 </a>
