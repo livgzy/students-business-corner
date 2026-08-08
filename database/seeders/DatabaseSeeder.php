3e29ed3d52db271f740353e96291fa1ecf0dde06
Liv <liv.gzy@gmail.com>
2026-08-09 01:02:07 +0700

    fixit3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -71,7 +71,7 @@ class DatabaseSeeder extends Seeder
             'username' => "admin",
             'email' => "admin@gmail.com",
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ]);
         
